999488b6bf21b3e29e8068c28edac34b8a1219af
vaibhavkothari33 <vaibhavkothari50@gmail.com>
2025-05-25 00:23:35 +0530

    time control

--- 05mytime/main.go
+++ 05mytime/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main(){
-	 
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Welcome to time study of golang")
+
+	presentTime := time.Now()
+	fmt.Println(presentTime)
+	fmt.Println("Formated Time")
+	fmt.Println(presentTime.Format("01-02-2006 15:04:05 Monday")) 
+
+	createdDate:=time.Date(2020,time.January,19,23,23,0,0,time.UTC)
+	fmt.Println(createdDate)
+	fmt.Println(createdDate.Format("01-02-2006 Monday"))
+
+	// date time week
 }
