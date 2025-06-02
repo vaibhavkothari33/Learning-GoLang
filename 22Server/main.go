ecc208230a1bc2d2a8134967a349ee2618683417
vaibhavkothari33 <vaibhavkothari50@gmail.com>
2025-06-02 22:43:31 +0530

    get request in golang comparsion through js server

--- 22Server/main.go
+++ 22Server/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
 func main() {
+	fmt.Println("welcome to web server")
+	PerformGetRequest()
+}
+
+func PerformGetRequest(){
+	const myurl = "http://localhost:8000"
+
+	responce,err:= http.Get(myurl)
+
+	if err !=nil{
+		panic(err)
+	}
+
 	
+	defer responce.Body.Close()
+
+	fmt.Println("Status Code :",responce.StatusCode)
+	fmt.Println("Status :",responce.Status)
+	fmt.Println("Content length :",responce.ContentLength)
+
+	content,_:=io.ReadAll(responce.Body)
+	fmt.Println(string(content))
+	fmt.Println(content)
 }
