91d12b44a515c68eccadaee8e8cfcef94d81670c
vaibhavkothari33 <vaibhavkothari50@gmail.com>
2025-06-04 21:23:14 +0530

    get , post, postform request in golang

--- 22Server/main.go
+++ 22Server/main.go
@@ -4,15 +4,19 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
 func main() {
 	fmt.Println("welcome to web server")
 	PerformGetRequest()
+	PerformPostJsonrequest()
+	PerformFormPostRequest()
 }
 
 func PerformGetRequest(){
-	const myurl = "http://localhost:8000"
+	const myurl = "http://localhost:8000/get"
 
 	responce,err:= http.Get(myurl)
 
@@ -27,7 +31,76 @@ func PerformGetRequest(){
 	fmt.Println("Status :",responce.Status)
 	fmt.Println("Content length :",responce.ContentLength)
 
-	content,_:=io.ReadAll(responce.Body)
+	// content,_:=io.ReadAll(responce.Body)
+	// fmt.Println(string(content))
+	// fmt.Println(content)
+
+	var responceString strings.Builder
+	content ,err := io.ReadAll(responce.Body)
+
+	if err !=nil{
+		panic(err)
+	}
+
+	byteCount ,_:= responceString.Write(content)
+
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println(byteCount)
+	fmt.Println(responceString.String())
+
+}
+
+
+func PerformPostJsonrequest(){
+	// send post request 
+	const myurl = "http://localhost:8000/post"
+
+
+	requestBody := strings.NewReader(`
+	{
+		"username" : "Vaibhav",
+		"age" : 19,
+		"address" : "Jaipur"
+	}`)
+
+	responce ,err :=http.Post(myurl, "application/json",requestBody)
+
+	if err != nil{
+		panic(err)
+	}
+
+	defer responce.Body.Close()
+
+	content, _:= io.ReadAll(responce.Body)
+
+	fmt.Println("")
+	fmt.Println("")
 	fmt.Println(string(content))
-	fmt.Println(content)
+	// fmt.Println("responce",)
+
+}
+
+func PerformFormPostRequest(){
+
+	const myurl = "http://localhost:8000/postform"
+
+	data := url.Values{}
+	data.Add("firstname","vaibhav")
+	data.Add("lastname","kothari")
+	data.Add("age","20")
+
+	responce,err :=http.PostForm(myurl,data)
+
+	if err!= nil {
+		panic(err)
+	}
+
+	defer responce.Body.Close()
+
+	content , _:= io.ReadAll(responce.Body)
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println(string(content))
+
 }
--- Readme.md
+++ Readme.md
@@ -356,3 +356,98 @@ func main() {
 	fmt.Println(anotherUrl)
 }
 ```
+
+# get request
+
+```go
+unc PerformGetRequest(){
+	const myurl = "http://localhost:8000/get"
+
+	responce,err:= http.Get(myurl)
+
+	if err !=nil{
+		panic(err)
+	}
+
+	
+	defer responce.Body.Close()
+
+	fmt.Println("Status Code :",responce.StatusCode)
+	fmt.Println("Status :",responce.Status)
+	fmt.Println("Content length :",responce.ContentLength)
+
+	// content,_:=io.ReadAll(responce.Body)
+	// fmt.Println(string(content))
+	// fmt.Println(content)
+
+	var responceString strings.Builder
+	content ,err := io.ReadAll(responce.Body)
+	byteCount ,_:= responceString.Write(content)
+
+	fmt.Println(byteCount)
+	fmt.Println(responceString.String())
+}
+```
+
+# post request
+
+```go
+func PerformPostJsonrequest(){
+	// send post request 
+	const myurl = "http://localhost:8000/post"
+
+
+	requestBody := strings.NewReader(`
+	{
+		"username" : "Vaibhav",
+		"age" : 19,
+		"address" : "Jaipur"
+	}`)
+
+	responce ,err :=http.Post(myurl, "application/json",requestBody)
+
+	if err != nil{
+		panic(err)
+	}
+
+	defer responce.Body.Close()
+
+	content, _:= io.ReadAll(responce.Body)
+
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println(string(content))
+	// fmt.Println("responce",)
+
+}
+
+```
+
+
+# post form  formencoded request
+
+```go
+func PerformFormPostRequest(){
+
+	const myurl = "http://localhost:8000/postform"
+
+	data := url.Values{}
+	data.Add("firstname","vaibhav")
+	data.Add("lastname","kothari")
+	data.Add("age","20")
+
+	responce,err :=http.PostForm(myurl,data)
+
+	if err!= nil {
+		panic(err)
+	}
+
+	defer responce.Body.Close()
+
+	content , _:= io.ReadAll(responce.Body)
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println(string(content))
+
+}
+```
