6fe3755848b4357e26c749b11937ca2e695d292a
vaibhavkothari33 <vaibhavkothari50@gmail.com>
2025-06-06 00:44:36 +0530

    netflix backend

---
+++ 26mongoAPI/controller/controller.go
@@ -0,0 +1,69 @@
+package controller
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/vaibhavkothari33/mongoapi/model"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+const connectionString = "mongodb+srv://vaibhavkothari50:iZqTxT76IgSuoh9k@gobackend.jbas9pp.mongodb.net/"
+
+const dbName = "netflix"
+const colName = "watchlist"
+
+// MI
+var collection *mongo.Collection
+
+// connect with mongo
+
+func init() {
+	// client option
+	clientOption := options.Client().ApplyURI(connectionString)
+
+	client, err := mongo.Connect(context.TODO(), clientOption)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Mongo DB connection Success")
+
+	collection = client.Database(dbName).collection(colName)
+
+	// instance
+	fmt.Println("collection instance is ready")
+}
+
+// mongo db helpers - file
+//insert one record
+
+func insertOneMovie(movie model.Netflix) {
+	inserted, err := collection.InsertOne(context.Background(), movie)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Inserted one movie in db with id:", inserted.InsertedID)
+}
+
+func updateOneRecord(moviedId string) {
+	id, err := primitive.ObjectIDFromHex // convert string into object id
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filter := bson.M{"_id":id}
+	update := bson.M{"$set": bson.M{"watched": true}}
+
+	result, err := collection.updateOne(context.Background(), filter, update)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("modified count : ", result.ModifiedCount)
+	// .m and .d when ordered then .m
+}
---
+++ 26mongoAPI/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Mongo db api")
+}
+
+// mongodb+srv://vaibhavkothari50:iZqTxT76IgSuoh9k@gobackend.jbas9pp.mongodb.net/
